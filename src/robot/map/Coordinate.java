5edaa56ebf41644d85d899c681e90045884feabe
Alex Konradi <akonradi@mit.edu>
2014-01-27 02:44:14 -0500

    Add convenience methods to robot.map.Coordinate

--- src/robot/map/Coordinate.java
+++ src/robot/map/Coordinate.java
@@ -1,47 +1,62 @@
 package robot.map;
 
-public class Coordinate{
-	    public final double x;
-	    public final double y;
+public class Coordinate {
+	public static final Coordinate ORIGIN = new Coordinate(0, 0);
+    public final double x;
+    public final double y;
 
-	    public Coordinate(double x, double y) {
-	        this.x = x;
-	        this.y = y;
-	    }
-	    
-	    public Coordinate(Coordinate coord){
-	    	this.x = coord.x;
-	    	this.y = coord.y;
-	    }
-	    
-	    public double distanceFrom(Coordinate other){
-	    	return Math.sqrt(Math.pow(this.x - other.x, 2.0) 
-	    			       + Math.pow(this.y - other.y, 2.0));
-	    }
-	    
-	    public Coordinate vectorTo(Coordinate dest){
-	    	return new Coordinate(dest.x - this.x,
-	    						  dest.y - this.y);	
-	    }
-	    
-	    public double angleBetween(Coordinate other){
-	    	return Math.acos((this.x*other.x + this.y*other.y)/
-	    			          (Math.sqrt(this.x*this.x + this.y*this.y) +
-	    			           Math.sqrt(other.x*other.x + other.y*other.y)));
-	    }
 
-	    @Override
-	    public boolean equals(Object o) {
-	        if (this == o) { // quick check
-	            return true;
-	        }
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Coordinate(Coordinate coord){
+    	this.x = coord.x;
+    	this.y = coord.y;
+    }
+    
+    public double distanceFrom(Coordinate other){
+    	return Math.sqrt(Math.pow(this.x - other.x, 2.0) 
+    			       + Math.pow(this.y - other.y, 2.0));
+    }
+    
+    public Coordinate vectorTo(Coordinate dest){
+    	return new Coordinate(dest.x - this.x,
+    						  dest.y - this.y);	
+    }
+    
+    public double angleBetween(Coordinate other){
+    	return Math.acos((this.x*other.x + this.y*other.y)/
+    			          (Math.sqrt(this.x*this.x + this.y*this.y) +
+    			           Math.sqrt(other.x*other.x + other.y*other.y)));
+    }
 
-	        if (o == null || !(o instanceof Coordinate)) {
-	            return false;
-	        }
+	public double angleFromOriginRad() {
+		return Math.atan2(this.y, this.x);
+	}
 
-	        double otherFirst = ((Coordinate) o).x;
-	        double otherSecond = ((Coordinate) o).y;
-	        return this.x == otherFirst && this.y == otherSecond;
-	    }
+	public double angleFromOriginDeg() {
+		return 180. / Math.PI * angleFromOriginRad();
+	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { // quick check
+            return true;
+        }
+
+        if (o == null || !(o instanceof Coordinate)) {
+            return false;
+        }
+
+        double otherFirst = ((Coordinate) o).x;
+        double otherSecond = ((Coordinate) o).y;
+        return this.x == otherFirst && this.y == otherSecond;
+    }
+
+	@Override
+	public String toString() {
+		return String.format("(%.2f, %.2f)", this.x, this.y);
+	}
 }
